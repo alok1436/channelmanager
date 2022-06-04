2334208ecf92570931d07e231143197f9cd19825
Alok Singh <alok.singh3286@gmail.com>
2022-06-04 18:49:57 +0530

    changes applied

--- app/Http/Controllers/EbayController.php
+++ app/Http/Controllers/EbayController.php
@@ -44,13 +44,23 @@ class EbayController extends Controller {
     {
         $channel = Channel::find($id); //dd($channel);
         if($channel){
+            session()->put('channel', $channel->idchannel);
             $url = "https://auth.ebay.com/oauth2/authorize?client_id=".$channel->appid."&response_type=code&redirect_uri=ottavio_linzalo-ottaviol-testap-gipawcx&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.notification.subscription https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly";
             return redirect($url);
         }
     }
 
     public function callback(Request $request)
-    {
-    	dd($request->all());
+    {   
+        if($request->filled('code')){
+            $idchannel = session()->get('channel');
+            $channel = Channel::find($idchannel);
+            $channel->ebaycode = $request->code;
+            $channel->save();
+
+            return redirect('channelView')->with('success','Connected successfully');
+        }else{
+            return redirect('channelView')->with('error','Something went wrong.');
+        }
     }
 }
